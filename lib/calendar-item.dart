cfdc3e3d6fbdee245d39dc13c529ad67b79f9fdb
d0rianb <dorian.beauchesne@icloud.com>
2023-12-06 15:46:42 +0100

    Improve login view

--- lib/calendar-item.dart
+++ lib/calendar-item.dart
@@ -16,60 +16,64 @@ class CalendarItem extends Hero {
   CalendarItem(this.event, this.size, this.contextSize, this.expanded)
       : super(
           tag: event.id,
-          child: Visibility(
-            visible: event.shouldDisplay,
-            child: DefaultTextStyle(
-              style: const TextStyle(
-                decoration: TextDecoration.none,
-                color: textColorDarkMode,
+          transitionOnUserGestures: true,
+          child: DefaultTextStyle(
+            style: const TextStyle(
+              decoration: TextDecoration.none,
+              color: textColorDarkMode,
+            ),
+            child: Center(
+              child: ConstrainedBox(
+              constraints: new BoxConstraints(
+                  minHeight: size.height,
+                  maxHeight: contextSize.height * 0.8, // 80% of the screen height
+                  maxWidth: size.width,
               ),
-              child: Center(
-                child: ConstrainedBox(
-                constraints: new BoxConstraints(maxHeight: contextSize.height * 0.9),
-                  child: FittedBox(
-                    child: Container(
-                      padding: expanded ? const EdgeInsets.all(10.0) : const EdgeInsets.all(2.0),
-                      margin: const EdgeInsets.all(0.0),
-                      width: size.width,
-                      height: expanded ? null : size.height,
-                      alignment: Alignment.center,
-                      clipBehavior: Clip.antiAlias,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        color: event.color,
-                        borderRadius: BorderRadius.all(Radius.circular(expanded ? 6.0 : 3.5)),
-                        border: Border.all(color: event.borderColor, width: expanded ? 7.0 : 2.5),
-                      ),
-                      child: SingleChildScrollView(
-                        controller: ScrollController(),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: expanded ? largePadding : nullPadding,
-                              child: Text(
-                                event.course.length > 0 ? event.course : event.subject,
-                                textAlign: TextAlign.center,
-                                maxLines: expanded ? null : 4, // The text is truncated after 4 lines when not expanded
-                                overflow: expanded ? null : TextOverflow.ellipsis,
-                                style: TextStyle(fontWeight: FontWeight.w800, fontSize: expanded ? 24 : 10),
+                child: FittedBox(
+                  child: Container(
+                    padding: expanded ? const EdgeInsets.all(10.0) : const EdgeInsets.all(2.0),
+                    margin: const EdgeInsets.all(0.0),
+                    width: size.width,
+                    height: size.height,
+                    alignment: Alignment.center,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: event.color,
+                      borderRadius: BorderRadius.all(Radius.circular(expanded ? 6.0 : 3.5)),
+                      border: Border.all(color: event.borderColor, width: expanded ? 7.0 : 2.5),
+                    ),
+                    child: SingleChildScrollView(
+                      controller: ScrollController(),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: expanded ? largePadding : nullPadding,
+                            child: Text(
+                              event.course.length > 0 ? event.course : event.subject,
+                              textAlign: TextAlign.center,
+                              maxLines: expanded ? null : 4, // The text is truncated after 4 lines when not expanded
+                              overflow: expanded ? null : TextOverflow.ellipsis,
+                              style: TextStyle(fontWeight: FontWeight.w800, fontSize: expanded ? 24 : 10),
+                            ),
+                          ),
+                          Padding(
+                            padding: (expanded && event.formattedLocation.length > 0) ? largePadding : regularPadding,
+                            child: Text(
+                              event.formattedLocation,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontStyle: FontStyle.italic,
+                                fontSize: expanded ? 22 : 9,
                               ),
                             ),
-                            // SizedBox(height: expanded ? 1 : 0),
-                            Padding(
-                              padding: (expanded && event.formattedLocation.length > 0) ? largePadding : regularPadding,
-                              child: Text(
-                                event.formattedLocation,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontStyle: FontStyle.italic,
-                                  fontSize: expanded ? 22 : 9,
-                                ),
-                              ),
-                            ),
-                            // The elements that are not visible when the widget is not expanded
-                            Visibility(visible: expanded, child: Column(
+                          ),
+                          // The elements that are not visible when the widget is not expanded
+                          SizedBox(
+                            height: expanded ? null : 0,
+                            child: Column(
                               children: [
                                 Padding(
                                   padding: expanded ? regularPadding : nullPadding,
@@ -112,9 +116,9 @@ class CalendarItem extends Hero {
                                   ),
                                 ),
                               ],
-                            ))
-                          ],
-                        ),
+                            ),
+                          )
+                        ],
                       ),
                     ),
                   ),
